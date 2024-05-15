0c1bb8f6cc950e7b6c368b30593e5804ab304442
zahroanisa <zahroanisatama@gmail.com>
2024-05-15 12:36:02 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use illuminate\Pagination\Paginator;
+
+use Illuminate\Support\ServiceProvider;
+
+use illuminate\Support\Facades\Storage;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Paginator::useBootstrapFive();
+    }
+}
